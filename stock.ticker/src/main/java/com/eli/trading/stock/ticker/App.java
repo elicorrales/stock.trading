2a3f3d00617cfe5a2dae760d6699764a63dd9d0a
Eli <e_corrales2003@yahoo.com>
2020-05-02 14:20:05 -0400

    first commit

---
+++ stock.ticker/src/main/java/com/eli/trading/stock/ticker/App.java
@@ -0,0 +1,13 @@
+package com.eli.trading.stock.ticker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
